dcef8f168f9ee4fda36eccde5309943155107940
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-10-23 11:09:15 +0200

    update data classes

--- app/src/main/java/com/susanafigueroa/yournameroom/data/UserDao.kt
+++ app/src/main/java/com/susanafigueroa/yournameroom/data/UserDao.kt
@@ -16,7 +16,7 @@ interface UserDao {
     @Update
     suspend fun update(user: User)
 
-    @Query("SELECT name from users WHERE id = :id")
+    @Query("SELECT * from users WHERE id = :id")
     fun getUser(id: Int): Flow<User?>
 
 }
