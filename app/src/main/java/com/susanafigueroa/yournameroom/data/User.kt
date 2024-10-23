593352c5ea047f4d94265c722cda22b1b8b136b7
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-10-23 09:19:46 +0200

    implement UserDao interface

--- app/src/main/java/com/susanafigueroa/yournameroom/data/User.kt
+++ app/src/main/java/com/susanafigueroa/yournameroom/data/User.kt
@@ -1,8 +1,11 @@
 package com.susanafigueroa.yournameroom.data
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
-@Entity(tableName = "user")
+@Entity(tableName = "users")
 data class User (
+    @PrimaryKey
+    val id: Int = 0,
     val name: String
 )
---
+++ app/src/main/java/com/susanafigueroa/yournameroom/data/UserDao.kt
@@ -0,0 +1,22 @@
+package com.susanafigueroa.yournameroom.data
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface UserDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(user: User)
+
+    @Update
+    suspend fun update(user: User)
+
+    @Query("SELECT name from users WHERE id = :id")
+    fun getUser(id: Int): Flow<User>
+
+}
