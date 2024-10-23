668495195b402a46451d7c6dc2257cbdf136fc88
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-10-23 12:21:47 +0200

    add YourNameRoomApplication to AndroidManifest

--- app/src/main/java/com/susanafigueroa/yournameroom/ui/home/YourNameRoomApp.kt
+++ app/src/main/java/com/susanafigueroa/yournameroom/ui/home/YourNameRoomApp.kt
@@ -11,16 +11,21 @@ import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.susanafigueroa.yournameroom.ui.AppViewModelProvider
 import com.susanafigueroa.yournameroom.ui.home.YourNameRoomViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -45,6 +50,8 @@ fun YourNameRoomApp(
             )
         }
     ) { contentPadding ->
+        var newNameUser by remember { mutableStateOf(userName) }
+
         Column(
             modifier = Modifier
                 .padding(contentPadding)
@@ -52,6 +59,16 @@ fun YourNameRoomApp(
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+
+            TextField(
+                value = newNameUser,
+                onValueChange = {
+                    newNameUser = it
+                },
+                label = {
+                    Text(stringResource(R.string.enter_your_name))
+                }
+            )
 
             Spacer(modifier = Modifier.height(20.dp))
 
